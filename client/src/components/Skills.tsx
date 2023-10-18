69555d40d02f6805e91853a9f20ec724dae0db31
yash patel <yashpatel21102002@gmail.com>
2023-10-18 12:56:22 +0530

    day2 oka

--- client/src/components/Skills.tsx
+++ client/src/components/Skills.tsx
@@ -12,48 +12,48 @@ export default function Skills(){
                 <div className="grid gap-7 grid-cols-9 max-lg:grid-cols-4 max-md:grid-cols-3">
                     <div className="flex flex-col justify-center items-center text-3xl cursor-pointer hover:text-dark-green ">
                         <BsTwitter />
-                        <span className='text-sm font-thin'>Twitter</span>
+                        <span className='text-sm font-md'>Twitter</span>
 
                     </div>
                     <div className="flex flex-col justify-center items-center text-3xl cursor-pointer hover:text-dark-green ">
                         <AiOutlineGithub />
-                        <span className='text-sm font-thin'>Github</span>
+                        <span className='text-sm font-md'>Github</span>
 
                     </div>
                     <div className="flex flex-col justify-center items-center text-3xl cursor-pointer hover:text-dark-green ">
                         <AiFillMediumSquare />
-                        <span className='text-sm font-thin'>Medium</span>
+                        <span className='text-sm font-md'>Medium</span>
 
                     </div>
                     <div className="flex flex-col justify-center items-center text-3xl cursor-pointer hover:text-dark-green ">
                         <AiFillLinkedin />
-                        <span className='text-sm font-thin'>Linkedin</span>
+                        <span className='text-sm font-md'>Linkedin</span>
 
                     </div>
                     <div className="flex flex-col justify-center items-center text-3xl cursor-pointer hover:text-dark-green ">
                         <AiFillInstagram />
-                        <span className='text-sm font-thin'>Instagram</span>
+                        <span className='text-sm font-md'>Instagram</span>
 
                     </div>
                     <div className="flex flex-col justify-center items-center text-3xl cursor-pointer hover:text-dark-green ">
                         <SiCodechef
                          />
-                        <span className='text-sm font-thin'>Codechef</span>
+                        <span className='text-sm font-md'>Codechef</span>
 
                     </div>
                     <div className="flex flex-col justify-center items-center text-3xl cursor-pointer hover:text-dark-green ">
                         <SiCodeforces />
-                        <span className='text-sm font-thin'>Codeforces</span>
+                        <span className='text-sm font-md'>Codeforces</span>
 
                     </div>
                     <div className="flex flex-col justify-center items-center text-3xl cursor-pointer hover:text-dark-green ">
                         <SiLeetcode />
-                        <span className='text-sm font-thin'>Leetcode</span>
+                        <span className='text-sm font-md'>Leetcode</span>
 
                     </div>
                     <div className="flex flex-col justify-center items-center text-3xl cursor-pointer hover:text-dark-green ">
                         <RxVercelLogo />
-                        <span className='text-sm font-thin'>Vercel</span>
+                        <span className='text-sm font-md'>Vercel</span>
 
                     </div>
                 </div>
